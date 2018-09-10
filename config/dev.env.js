8700066b7e24fba8547bc8d52a1fde46952d46c7
Luis Felipe Nicolau <luisfnicolau@hotmail.com>
2018-09-10 19:42:35 -0300

    Test

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // ROOT_API: '"http://api.colonia.ferias/"'
-  ROOT_API: '"http://colonia.voltzparkour.com.br/api/"'
+  ROOT_API: '"http://api.colonia.ferias/"'
+  // ROOT_API: '"http://colonia.voltzparkour.com.br/api/"'
 })
